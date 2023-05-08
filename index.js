a3c1e79e7c804e2822bb4982caf7225bb25a50a3
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-08 18:32:34 +0800

    able to display timer and do the countdown

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //javascript.js
 var playing = false;
 var score;
-
+var action;
+var timeremaining;
 
 //if we click on the start/reset
 
@@ -47,4 +48,43 @@ document.getElementById("startreset").onclick = function(){
         generateQA();
     }
     
+}
+
+//functions
+
+//start counter
+
+function startCountdown(){
+    action = setInterval(function(){
+        timeremaining -= 1;
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+        if(timeremaining == 0){// game over
+            stopCountdown();
+            show("gameOver");
+         document.getElementById("gameOver").innerHTML = "<p>Game over!</p><p>Your score is " + score + ".</p>";   
+            hide("timeremaining");
+            hide("correct");
+            hide("wrong");
+            playing = false;
+            document.getElementById("startreset").innerHTML = "Start Game";
+        }
+    }, 1000);    
+}
+
+//stop counter
+
+function stopCountdown(){
+    clearInterval(action);   
+}
+
+//hide an element
+
+function hide(Id){
+    document.getElementById(Id).style.display = "none";   
+}
+
+//show an element
+
+function show(Id){
+    document.getElementById(Id).style.display = "block";   
 }
